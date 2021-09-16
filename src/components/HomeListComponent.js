73827eb19e529d75875c37900dc3534a54c52368
angelz510 <angelzrod4@gmail.com>
2021-09-16 14:19:10 -0700

    created search component and made axios call

---
+++ src/components/HomeListComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeListComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeListComponent
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Input = styled.input`
+  margin: 0;
+  padding: 0;
+  height: 45px;
+  width: 300px;
+  outline: none;
+  border: none;
+  background: none;
+  font-size: 25px;
+`
+
+const Button = styled.button`
+  color: gray;
+  height: 45px;
+  width: 60px;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+`
+
+const SearchComponent = (props) => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const onChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    props.searchAnime(searchTerm)
+  }
+
+  return (
+    <Container onSubmit={onSubmit}>
+      <Input onChange={onChange}/>
+      <Button type='submit'>Search</Button>
+    </Container>
+  )
+}
+
+export default SearchComponent
