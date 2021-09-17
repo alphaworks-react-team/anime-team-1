62eb872cc24625c29ae69075a287beafe567b50c
andrewjspivey <12spivey@gmail.com>
2021-09-17 10:50:44 -0700

    adds fetches for home page in utils/fetches and adds the cards for popular,trending,ranked on homepage

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-=======
-import './App.css';
-import axios from 'axios';
-import SearchComponent from './components/SearchComponent';
-import Home from './Pages/Home';
+import axios from "axios";
+import SearchComponent from "./components/SearchComponent";
+import Home from "./Pages/Home";
 
 function App() {
   const searchAnime = (searchTerm) => {
     axios
       .get(`https://kitsu.io/api/edge/anime?filter[text]=${searchTerm}`, {
         headers: {
-          "Accept": "application/vnd.api+json",
-          "Content-Type": "application/vnd.api+json"
-        }
+          Accept: "application/vnd.api+json",
+          "Content-Type": "application/vnd.api+json",
+        },
       })
       .then((res) => {
         console.log(res);
       })
-      .catch((err) => {console.log(err)})
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="App">
-      <SearchComponent searchAnime={searchAnime}/>
-      <Home/>
+      <SearchComponent searchAnime={searchAnime} />
+      <Home />
     </div>
   );
->>>>>>> main
 }
 
 export default App;
--- src/components/HomeListComponent.js
+++ src/components/HomeListComponent.js
@@ -1,41 +1,67 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Container } from '../fragments/Container'
-import Card from './Card'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container } from "../fragments/Container";
+import { getTrendingAnime, getPopular, getRanked } from "../utils/fetches";
+import Card from "./Card";
 
 const HomeListComponent = () => {
-  const [ trending, setTrending ] = useState([])
+  const [trending, setTrending] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [highestRanked, setHighestRanked] = useState([]);
 
-  const searchTrendingAnime = () => {
-    axios
-      .get('https://kitsu.io/api/edge/trending/anime?[limit]=5', {
-        headers: {
-          "Accept": "application/vnd.api+json",
-          "Content-Type": "application/vnd.api+json"
-        }
-      })
-      .then((res) => {
-        const data = res.data.data
-        setTrending(data);
-      })
-      .catch((err) => {console.log(err)})
-  }
-  console.log(trending)
+  const fetchAndSetTrending = async () => {
+    const res = await getTrendingAnime();
+    const data = await res;
+    setTrending(data);
+  };
+  const fetchAndSetPopular = async () => {
+    const res = await getPopular();
+    const data = await res;
+    setPopular(data);
+  };
+  const fetchAndSetRanked = async () => {
+    const res = await getRanked();
+    const data = await res;
+    setHighestRanked(data);
+  };
+
   useEffect(() => {
-    searchTrendingAnime()
-  }, [])
+    fetchAndSetTrending();
+    fetchAndSetPopular();
+    fetchAndSetRanked();
+  }, []);
 
   return (
-    <Container>
-      {trending.map((card, index) => (
-        <Card key={index}
-          img={card.attributes.posterImage.small}
-          title={card.attributes.titles.en}
-          >
-        </Card>
-      ))}
-    </Container>
-  )
-}
+    <div>
+      <Container>
+        {trending.map((card, index) => (
+          <Card
+            key={index}
+            img={card.attributes.posterImage.small}
+            title={card.attributes.titles.en}
+          ></Card>
+        ))}
+      </Container>
+      <Container>
+        {popular.map((card, index) => (
+          <Card
+            key={index}
+            img={card.attributes.posterImage.small}
+            title={card.attributes.titles.en}
+          ></Card>
+        ))}
+      </Container>
+      <Container>
+        {highestRanked.map((card, index) => (
+          <Card
+            key={index}
+            img={card.attributes.posterImage.small}
+            title={card.attributes.titles.en}
+          ></Card>
+        ))}
+      </Container>
+    </div>
+  );
+};
 
-export default HomeListComponent
+export default HomeListComponent;
